bb34ac9696553d0913c820e9a991ca61ddcc875e
Lohanna-M <lohannamaria47@gmail.com>
2024-10-10 15:29:39 -0300

    Admin-dashboard

--- resources/views/admindashboard.blade.php
+++ resources/views/admindashboard.blade.php
@@ -5,28 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset( 'css/admindashboard.css') }}">
-<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Admin-Dashboard</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="title" href="#">SimpleBlog</a>
+          <a class="title-navbar" href="#">SimpleBlog</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <button class="button">Home</button>
-                <button class="button">Criar Post</button>
+                <button class="button">Novo Post</button>
                 <button class="button">Meus Posts</button>
             </ul>
-          </div>
+            </div>
         </div>
-      </nav>
+    </nav>
+    <div class="container">
+    <div class="card">
+     <div class="image"></div>
+      <div class="content">
+        <a href="#">
+          <span class="title">
+           Titulo
+          </span>
+        </a>
+
+        <p class="desc">
+         Desc
+        </p>
+
+        <button class="Like">
+            <svg height="32" width="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0H24V24H0z" fill="none"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"></path></svg>
+        </button>
+      </div>
+    </div>
+    </div>
 </body>
 </html>
